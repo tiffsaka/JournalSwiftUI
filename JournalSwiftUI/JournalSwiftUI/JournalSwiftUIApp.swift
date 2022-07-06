d180fea1009bd0a7abad191744fed23e750dfc4e
tiffsaka <iosdevtiffany@gmail.com>
2022-07-06 09:45:17 -0700

    Create project

---
+++ JournalSwiftUI/JournalSwiftUI/JournalSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalSwiftUIApp.swift
+//  JournalSwiftUI
+//
+//  Created by Tiffany Sakaguchi on 7/6/22.
+//
+
+import SwiftUI
+
+@main
+struct JournalSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
